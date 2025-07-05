a0015f40379d894f50fff8739d347da4c719b320
Gizmo900 <jahromia75@gmail.com>
2025-07-05 12:17:54 +0100

    Add files via upload

---
+++ Parsa Var 5th July.py
@@ -0,0 +1,20 @@
+
+
+Name = "Parsa"
+hobby = input("What is  your hobby?")
+print(hobby)
+age = int(input("how old are you?"))
+print(age)
+weight = float(input("how many kg do you weigh?"))
+print(weight,"kg")
+print(type(weight))
+print("My name is "+ Name+" . I am "+str(age)+" years old. I love "+ hobby)
+"""
+x=5
+y=int(input("enter a number"))
+sum1 = x+y
+print(sum1)
+firstname = input("what is your first name?")
+lastname = input("what is your last name?")
+print(firstname,lastname)
+"""
