4967ab4ecfa69442c26f7167dcb42c77eddbe929
Gizmo900 <jahromia75@gmail.com>
2025-07-12 12:04:41 +0100

    Add files via upload

---
+++ conditionals and operators.py
@@ -0,0 +1,12 @@
+day=input("What day is it?").lower()
+holiday=input("Is it holiday today?yes/no ").upper()
+print(day)
+if day == "saturday":
+    print("python classes")
+
+elif day == "monday" and holiday=="YES":
+    print("you can stay at home")
+else:
+    print("its a school day")
+     
+
